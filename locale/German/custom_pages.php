5dac4d44ef1f74087d5bb461be99e9c7baec98c0
RobiNN <kelcakrobo@gmail.com>
2020-12-06 16:46:30 +0100

    Delete locales

--- locale/German/custom_pages.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['400'] = "Du hast nicht die Erlaubnis um diese Seite anzusehen .";
-$locale['401'] = "Interner Fehler";
-$locale['402'] = "Nicht imstande, Seiteninhalt zurückzuholen.";
-?>
--- locale/Polish/custom_pages.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['400'] = "Nie masz uprawnieñ, aby ogl±daæ tê czê¶æ witryny.";
-$locale['401'] = "B³±d";
-$locale['402'] = "Nie uda³o siê odczytaæ zawarto¶ci strony.";
-?>

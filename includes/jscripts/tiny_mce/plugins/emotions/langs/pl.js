8a8cb3e5aec28f0145638e3c013a13dddde73c72
RobiNN <kelcakrobo@gmail.com>
2020-12-06 22:01:13 +0100

    Add v6.01.16

---
+++ includes/jscripts/tiny_mce/plugins/emotions/langs/pl.js
@@ -0,0 +1,24 @@
+// PL lang variables
+// fixed by Wooya
+// http://www.mfusion.prv.pl
+
+tinyMCE.addToLang('emotions',{
+title : 'Wstaw emotikonkê',
+desc : 'Emotikonki',
+cool : 'Cool',
+cry : 'P³acze',
+embarassed : 'Embarassed',
+foot_in_mouth : 'Laczek w gêbie',
+frown : 'Frown',
+innocent : 'Niewinny',
+kiss : 'Poca³unek',
+laughing : '¦miech',
+money_mouth : 'Money mouth',
+sealed : 'Sealed',
+smile : 'Smile',
+surprised : 'Surprised',
+tongue_out : 'Tongue out',
+undecided : 'Undecided',
+wink : 'Wink',
+yell : 'Yell'
+});

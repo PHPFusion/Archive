d3152b2f148828cff14ac23f7979a618e5d995a8
RobiNN <kelcakrobo@gmail.com>
2020-12-06 16:17:32 +0100

    Delete locales

--- language/Danish/comments.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("LAN_200", "Kommentarer");
-define("LAN_201", "Der er ikke skrevet kommentarer.");
-define("LAN_202", "Skriv en kommentar");
-define("LAN_203", "Navn:");
-define("LAN_204", "Kommentar:");
-define("LAN_205", "Du skal logge på for at skrive en kommentar.");
-?>

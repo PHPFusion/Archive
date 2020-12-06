35d2c209e20d4b25de30d3660dc9b5b507a3b42b
RobiNN <kelcakrobo@gmail.com>
2020-12-06 17:00:08 +0100

    Delete locales

--- locale/Polish/articles.php
+++
@@ -1,6 +0,0 @@
-<?php
-$locale['400'] = "Artyku³y";
-$locale['401'] = "Brak zdefiniowanej kategorii";
-$locale['402'] = "NOWY";
-$locale['403'] = "Brak artyku³ów w tej kategorii";
-?>
--- locale/Spanish/articles.php
+++
@@ -1,8 +0,0 @@
-<?php
-$locale['400'] = "Artículos";
-$locale['401'] = "No se definió ninguna 
-Categoría en Artículos";
-$locale['402'] = "Nuevo";
-$locale['403'] = "No se han añadido 
-Artículos a esta Categoría";
-?>

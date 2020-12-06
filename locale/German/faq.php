c329c9f4497159a8cc1080dc2fdc23ddaf99a710
RobiNN <kelcakrobo@gmail.com>
2020-12-06 17:05:48 +0100

    Delete locales

--- locale/German/faq.php
+++
@@ -1,7 +0,0 @@
-<?php
-$locale['400'] = "FAQ (Häufig gestellte Fragen)";
-$locale['401'] = "FAQ";
-// Downloads Notices
-$locale['410'] = "Keine FAQ Kategorien definiert.";
-$locale['411'] = "Keine FAQ's wurden in dieser Kategorie hinzugefügt.";
-?>
--- locale/Italian/print.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['400'] = "Inviato da ";
-$locale['401'] = " il ";
-$locale['402'] = "News estese";
-?>
